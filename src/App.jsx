c08d60f91d9fd7e09e77bce5209445562e71274f
Henryck <ryck302@gmail.com>
2022-05-30 01:36:39 -0300

    navBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import './App.css'
+import Navbar from './components/Navbar'
+import {BrowserRouter as Router} from 'react-router-dom'
 
 function App() {
   return (
-   <h1>Hey bro</h1>
+   <Router>
+     <Navbar />
+   </Router>
   )
 }
 
