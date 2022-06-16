9f6129d3d547601d93f7d09a73c36bbe971113d0
Henryck <ryck302@gmail.com>
2022-06-15 22:03:10 -0300

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import {BrowserRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages'
+import SigninPage from './pages/signin'
 
 import './App.css'
 
 function App() {
   return (
    <Router>
-     <Home />
+     <Routes>
+        <Route path='/' element={<Home />} exact />
+        <Route path='/signin' element={<SigninPage />} exact />
+     </Routes>
    </Router>
   )
 }
 
-export default App
+export default App;   
---
+++ src/pages/signin.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SigninPage = () => {
+  return (  
+    <h1>Sign in page</h1>
+  );
+}
+ 
+export default SigninPage;
