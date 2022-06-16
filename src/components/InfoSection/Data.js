ac45f8f7f909aed649515128b413d07c1da89dd7
Henryck <ryck302@gmail.com>
2022-06-15 21:38:03 -0300

    services section

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -20,35 +20,35 @@ export const homeObjOne = {
 }
 
 export const homeObjTwo = {
-  id: 'about',
+  id: 'discover',
   lightBg: true,
   lightText: false,
-  lightTextDesc: true,
-  topLine: 'Premium Bank',
-  headline: 'Unlimited Transactions with zero fees',
-  description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees',
-  buttonLabel: 'Get started',
+  lightTextDesc: false,
+  topLine: 'Unlimited Access',
+  headline: 'Login to your account at any time',
+  description: 'We have you covered no matter where you are located. All you need is an internet connection and a phone or computer.',
+  buttonLabel: 'Learn more',
   imgStart: true,
   img: imageObjTwo,
-  alt: 'Car',
-  dark: true,
-  primary: true,
+  alt: 'Piggybank',
+  dark: false,
+  primary: false,
   darkText: true
 }
 
 export const homeObjThree = {
-  id: 'about',
-  lightBg: false,
-  lightText: true,
-  lightTextDesc: true,
-  topLine: 'Premium Bank',
-  headline: 'Unlimited Transactions with zero fees',
-  description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees',
-  buttonLabel: 'Get started',
+  id: 'signup',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Join our Team',
+  headline: 'Creating an account is extremly easy',
+  description: "Get everything set yp and ready in under 10 minutes. All you need to do is add your information and you're ready to go",
+  buttonLabel: 'Start Now',
   imgStart: false,
   img: imageObjThree,
-  alt: 'Car',
-  dark: true,
-  primary: true,
-  darkText: false
+  alt: 'Paper',
+  dark: false,
+  primary: false,
+  darkText: true
 }
